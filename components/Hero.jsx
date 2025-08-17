49272851c882a046471c7f68dbe8757967d53bcc
Ignatius_Reilly <dmitriy_xix@hotmail.com>
2025-08-17 04:35:45 -0400

    starting #33

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Hero() {
   return (
     <div className="hero-section">
@@ -5,8 +6,7 @@ export default function Hero() {
       <div>
         <p className="header-text">
           Stay on top <br />
-          of your <br />
-          bills!
+          of your bills!
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -17,20 +17,25 @@ export default function Hero() {
             <path d="M80-160v-640h800v640H80Zm80-80h640v-480H160v480Zm0 0v-480 480Zm160-40h80v-40h40q17 0 28.5-11.5T480-360v-120q0-17-11.5-28.5T440-520H320v-40h160v-80h-80v-40h-80v40h-40q-17 0-28.5 11.5T240-600v120q0 17 11.5 28.5T280-440h120v40H240v80h80v40Zm320-30 80-80H560l80 80Zm-80-250h160l-80-80-80 80Z" />
           </svg>
         </p>
-        <p className="text-medium">Measure, analyze and track your bills.</p>
-        <button>
-          <h5>Get started &rarr;</h5>
-        </button>
+        <p className="text-medium">
+          Measure, analyze and track your bills. Measure, analyze and track your
+          bills. Measure, analyze and track your bills!
+        </p>
+        <Link href={"/dashboard"}>
+          <button>
+            <h5>Get started &rarr;</h5>
+          </button>
+        </Link>
         <small>Sign up for free, no ads.</small>
       </div>
 
-      {/* and this div is for the visual display */}
-      <div classname="hero-display">
+      {/* is for the visual display */}
+      <div className="hero-display">
         {/* column 1 */}
         <div>
           <div className="card">
             <div>
-              <div className="card-reader"></div>
+              <div className="card-header"></div>
               <div className="card-blank"></div>
               <div className="card-description-large" />
               <div className="card-description-small" />
@@ -39,7 +44,7 @@ export default function Hero() {
           </div>
           <div className="card">
             <div>
-              <div className="card-reader"></div>
+              <div className="card-header"></div>
               <div className="card-blank"></div>
               <div className="card-description-large" />
               <div className="card-description-small" />
@@ -52,7 +57,7 @@ export default function Hero() {
         <div>
           <div className="card">
             <div>
-              <div className="card-reader"></div>
+              <div className="card-header"></div>
               <div className="card-blank"></div>
               <div className="card-description-large" />
               <div className="card-description-small" />
@@ -61,7 +66,7 @@ export default function Hero() {
           </div>
           <div className="card">
             <div>
-              <div className="card-reader"></div>
+              <div className="card-header"></div>
               <div className="card-blank"></div>
               <div className="card-description-large" />
               <div className="card-description-small" />
